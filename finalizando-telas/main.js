3067560008314fc93d612fb2b78526231e07c5f4
Samuel Cristian <samuel.cristian17062004@gmail.com>
2023-08-15 15:07:00 -0300

    finish task

--- finalizando-telas/main.js
+++ finalizando-telas/main.js
@@ -1,21 +1,52 @@
-const randomNumber = Math.round(Math.random() * 10);
+let randomNumber = Math.round(Math.random() * 10);
+const btnTry = document.querySelector("#btnTry");
+const bntReset = document.querySelector("#btnReset");
+const screen1 = document.querySelector(".screen1");
+const screen2 = document.querySelector(".screen2 h2");
 
 let xAttempts = 1;
 
-function handleClick(event) {
+function handleTryClick(event) {
   event.preventDefault();
-
   const inputNumber = document.querySelector("#inputNumber");
+
+  If(isValue());
+  {
+    alert("Please type a correct number");
+  }
 
   If(Number(inputNumber.value) == randomNumber);
   {
-    document.querySelector(".screen1").classList.add("hide");
-    document.querySelector(".screen2").classList.remove("hide");
+    toogleScreen();
 
-    document.querySelector(
-      ".screen2 h2"
-    ).innerText = `acertou em ${xAttempts} tentativas`;
+    screen2.innerText = `acertou em ${xAttempts} tentativas`;
   }
 
   xAttempts++;
+}
+
+function handleResetClick(event) {
+  toogleScreen();
+  xAttempts = 1;
+}
+
+function toogleScreen(event) {
+  screen1.classList.toggle("hide");
+  screen2.classList.toggle("hide");
+  randomNumber = Math.round(Math.random() * 10);
+}
+
+btnTry.addEventListener("click", handleTryClick);
+bntReset.addEventListener("click", handleResetClick);
+document.addEventListener("keydown", (e) => {
+  if (e.key == "Enter" && screen1.classList.contains("hide")) {
+    handleResetClick();
+  }
+});
+
+function isValue() {
+  isValue = true;
+  if (inputNumber.value < 0 || inputNumber.value > 10) {
+    isValue = false;
+  }
 }
